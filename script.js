57e09ff9019e05e8f4e418a925b3f8da0453e8f8
Shobhit070304 <sharmashobhit1000@gmail.com>
2024-04-14 23:21:09 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,77 @@
+// <----------Project 1---------->
+
+// var add = document.querySelector('.remove');
+// var head = document.querySelector('h2')
+
+// let toggle = 0;
+// add.addEventListener('click', function () {
+//     if (toggle === 0) {
+//         head.innerHTML = "Friend"
+//         head.style.color = "green"
+//         add.innerHTML = 'Remove Friend'
+//         toggle = 1;
+//     }
+//     else {
+//         head.innerHTML = "Stranger"
+//         head.style.color = "red"
+//         add.innerHTML = 'Add Friend'
+//         toggle = 0
+//     }
+// })
+
+
+
+// <----------Project 2---------->
+
+// var btn = document.querySelector('i')
+// var img = document.querySelector('img')
+
+// img.addEventListener('dblclick', function () {
+//     btn.style.transform = "translate(-50%, -50%) scale(1)"
+//     btn.style.opacity = 0.6
+
+//     setTimeout(() => {
+//         btn.style.transform = "translate(-50%, -50%) scale(0)"
+//         btn.style.opacity = 0
+
+//     }, 1000);
+// })
+
+
+
+
+// <----------Project 3---------->
+
+// var main = document.querySelector('.main')
+// var crsr = document.querySelector('.curser')
+
+// main.addEventListener("mousemove", (e) => {
+//     let x = e.x + "px"
+//     let y = e.y + "px"
+
+//     crsr.style.left = x
+//     crsr.style.top = y
+// })
+
+
+
+
+// <----------Project 4---------->
+
+// var main = document.querySelectorAll(".elem")
+
+// main.forEach(function (val) {
+//     val.addEventListener('mouseenter', function () {
+//         val.childNodes[3].style.opacity = 1;
+//     })
+//     val.addEventListener('mouseleave', function () {
+//         val.childNodes[3].style.opacity = 0;
+//     })
+
+//     val.addEventListener('mousemove', function (e) {
+//         val.childNodes[3].style.left = e.x + "px";
+//         val.childNodes[3].style.top = e.y + "px";
+
+//     })
+// })
+
